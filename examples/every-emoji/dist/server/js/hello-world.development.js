d7e0300116bbe7f1be653eeaf395b262cfb7abaa
Jared Lunde <jared.lunde@gmail.com>
2019-03-20 18:46:04 -0600

    > perf + size in splitEmojis

--- examples/every-emoji/dist/server/js/hello-world.development.js
+++ examples/every-emoji/dist/server/js/hello-world.development.js
@@ -8256,33 +8256,23 @@ const categories = ['symbols', 'regional', 'flags', 'travel', 'people', 'nature'
 const spriteSizes = [[291, 1], [26, 1], [269, 1], [123, 1], [396, 1], [178, 1], [200, 1], [107, 1], [34, 34], [108, 1]];
 // CONCATENATED MODULE: ./node_modules/react-emoji-component/es/utils/split.js
 let CR = 0,
-    LF = 1,
-    Control = 2,
-    Extend = 3,
-    Regional_Indicator = 4,
-    SpacingMark = 5,
-    Other = 11,
-    E_Base = 13,
-    E_Modifier = 14,
-    ZWJ = 15,
-    Glue_After_Zwj = 16,
-    E_Base_GAZ = 17; // BreakTypes
+    Extend = 1,
+    RegionalIndicator = 2,
+    Other = 3,
+    EBase = 4,
+    EModifier = 5,
+    ZWJ = 6,
+    GlueAfterZWJ = 7,
+    EBaseGAZ = 8; // BreakTypes
 
 let NotBreak = 0,
     BreakStart = 1,
     Break = 2,
     BreakLastRegional = 3,
-    BreakPenultimateRegional = 4;
-
-const isSurrogate = (str, pos) => 0xd800 <= str.charCodeAt(pos) && str.charCodeAt(pos) <= 0xdbff && 0xdc00 <= str.charCodeAt(pos + 1) && str.charCodeAt(pos + 1) <= 0xdfff; // Private function, gets a Unicode code point from a JavaScript UTF-16 string
+    BreakPenultimateRegional = 4; // Private function, gets a Unicode code point from a JavaScript UTF-16 string
 // handling surrogate pairs appropriately
 
-
-const codePointAt = (str, idx) => {
-  if (idx === void 0) {
-    idx = 0;
-  }
-
+const codePointAt = (str, idx = 0) => {
   let code = str.charCodeAt(idx); // if a high surrogate
 
   if (0xD800 <= code && code <= 0xDBFF && idx < str.length - 1) {
@@ -8320,11 +8310,11 @@ function split_ref(c) {
 }
 
 function split_ref2(c) {
-  return c === Regional_Indicator;
+  return c === RegionalIndicator;
 }
 
 function split_ref3(c) {
-  return c === Regional_Indicator;
+  return c === RegionalIndicator;
 }
 
 function split_ref4(c) {
@@ -8336,70 +8326,62 @@ function _ref5(c) {
 }
 
 const shouldBreak = (start, mid, end) => {
-  let all = [start].concat(mid).concat([end]),
-      previous = all[all.length - 2],
-      next = end; // Lookahead termintor for:
-  // GB10. (E_Base | EBG) Extend* ?	E_Modifier
+  let all = [start];
+  all.push.apply(all, mid);
+  all.push(end);
+  let previous = all[all.length - 2],
+      next = end; // Lookahead terminator for:
+  // GB10. (EBase | EBG) Extend* ?	EModifier
 
-  let eModifierIndex = all.lastIndexOf(E_Modifier);
+  let eModifierIndex = all.lastIndexOf(EModifier);
 
-  if (eModifierIndex > 1 && all.slice(1, eModifierIndex).every(split_ref) && [Extend, E_Base, E_Base_GAZ].indexOf(start) === -1) {
+  if (eModifierIndex > 1 && Extend !== next && EBase !== next && EBaseGAZ !== next && all.slice(1, eModifierIndex).every(split_ref)) {
     return Break;
-  } // Lookahead termintor for:
+  } // Lookahead terminator for:
   // GB12. ^ (RI RI)* RI	?	RI
   // GB13. [^RI] (RI RI)* RI	?	RI
 
 
-  let rIIndex = all.lastIndexOf(Regional_Indicator);
+  let rIIndex = all.lastIndexOf(RegionalIndicator);
 
-  if (rIIndex > 0 && all.slice(1, rIIndex).every(split_ref2) && Regional_Indicator !== previous) {
+  if (rIIndex > 0 && all.slice(1, rIIndex).every(split_ref2) && RegionalIndicator !== previous) {
     if (all.filter(split_ref3).length % 2 === 1) {
       return BreakLastRegional;
     } else {
       return BreakPenultimateRegional;
     }
-  } // GB3. CR X LF
-
-
-  if (previous === CR && next === LF) {
-    return NotBreak;
   } // GB4. (Control|CR|LF) ÷
-  else if (previous === Control || previous === CR || previous === LF) {
-      if (next === E_Modifier && mid.every(split_ref4)) {
-        return Break;
-      } else {
-        return BreakStart;
-      }
-    } // GB5. ÷ (Control|CR|LF)
-    else if (next === Control || next === CR || next === LF) {
-        return BreakStart;
-      } // GB9. X (Extend|ZWJ)
-      else if (next === Extend || next === ZWJ) {
-          return NotBreak;
-        } // GB9a. X SpacingMark
-        else if (next === SpacingMark) {
-            return NotBreak;
-          } // GB10. (E_Base | EBG) Extend* ?	E_Modifier
+
+
+  if (previous === CR) {
+    return next === EModifier && mid.every(split_ref4) ? Break : BreakStart;
+  } // GB5. ÷ (Control|CR|LF)
+  else if (next === CR) {
+      return BreakStart;
+    } // GB9. X (Extend|ZWJ)
+    else if (next === Extend || next === ZWJ) {
+        return NotBreak;
+      } // GB10. (EBase | EBG) Extend* ?	EModifier
 
 
   let previousNonExtendIndex = all.indexOf(Extend) > -1 ? all.lastIndexOf(Extend) - 1 : all.length - 2;
 
-  if ([E_Base, E_Base_GAZ].indexOf(all[previousNonExtendIndex]) > -1 && all.slice(previousNonExtendIndex + 1, -1).every(_ref5) && next === E_Modifier) {
+  if ((EBase === all[previousNonExtendIndex] || EBaseGAZ === all[previousNonExtendIndex]) && all.slice(previousNonExtendIndex + 1, -1).every(_ref5) && next === EModifier) {
     return NotBreak;
-  } // GB11. ZWJ ? (Glue_After_Zwj | EBG)
+  } // GB11. ZWJ ? (GlueAfterZWJ | EBG)
 
 
-  if (previous === ZWJ && [Glue_After_Zwj, E_Base_GAZ].indexOf(next) > -1) {
+  if (previous === ZWJ && (next === GlueAfterZWJ || next === EBaseGAZ)) {
     return NotBreak;
   } // GB12. ^ (RI RI)* RI ? RI
   // GB13. [^RI] (RI RI)* RI ? RI
 
 
-  if (mid.indexOf(Regional_Indicator) > -1) {
+  if (mid.indexOf(RegionalIndicator) > -1) {
     return Break;
   }
 
-  if (previous === Regional_Indicator && next === Regional_Indicator) {
+  if (previous === RegionalIndicator && next === RegionalIndicator) {
     return NotBreak;
   } // GB999. Any ? Any
 
@@ -8423,42 +8405,28 @@ const nextBreak = (string, index) => {
 
   let prev = getGraphemeBreakProperty(codePointAt(string, index));
   let mid = [],
-      i = index + 1;
+      i = index + 1,
+      atPrev = string.charCodeAt(index);
 
   for (; i < string.length; i++) {
-    // check for already processed low surrogates
-    if (isSurrogate(string, i - 1)) {
+    const atCurr = string.charCodeAt(i); // check for already processed low surrogates
+
+    if ( // is surrogate
+    0xd800 <= atPrev && atPrev <= 0xdbff && 0xdc00 <= atCurr && atCurr <= 0xdfff) {
       continue;
     }
 
     let next = getGraphemeBreakProperty(codePointAt(string, i));
 
-    if (shouldBreak(prev, mid, next)) {
+    if (shouldBreak(prev, mid, next) !== NotBreak) {
       return i;
     }
 
     mid.push(next);
+    atPrev = atCurr;
   }
 
   return string.length;
-}; // Breaks the given string into an array of grapheme cluster strings
-
-
-const splitGraphemes = str => {
-  let res = [],
-      index = 0,
-      brk;
-
-  while ((brk = nextBreak(str, index)) < str.length) {
-    res.push(str.slice(index, brk));
-    index = brk;
-  }
-
-  if (index < str.length) {
-    res.push(str.slice(index));
-  }
-
-  return res;
 }; //given a Unicode code point, determines this symbol's grapheme break property
 
 
@@ -8471,58 +8439,13 @@ const getGraphemeBreakProperty = code => {
     return CR;
   }
 
-  if (0x000A === code) {
-    // Cc       <control-000A>
-    return LF;
-  }
-
-  if (0x0000 <= code && code <= 0x0009 || // Cc  [10] <control-0000>..<control-0009>
-  0x000B <= code && code <= 0x000C || // Cc   [2] <control-000B>..<control-000C>
-  0x000E <= code && code <= 0x001F || // Cc  [18] <control-000E>..<control-001F>
-  0x007F <= code && code <= 0x009F || // Cc  [33] <control-007F>..<control-009F>
-  0x200B === code || // Cf       ZERO WIDTH SPACE
-  0x200E <= code && code <= 0x200F || // Cf   [2] LEFT-TO-RIGHT MARK..RIGHT-TO-LEFT MARK
-  0x2028 === code || // Zl       LINE SEPARATOR
-  0x2029 === code || // Zp       PARAGRAPH SEPARATOR
-  0x202A <= code && code <= 0x202E || // Cf   [5] LEFT-TO-RIGHT EMBEDDING..RIGHT-TO-LEFT OVERRIDE
-  0x2060 <= code && code <= 0x2064 || // Cf   [5] WORD JOINER..INVISIBLE PLUS
-  0x2065 === code || // Cn       <reserved-2065>
-  0x2066 <= code && code <= 0x206F || // Cf  [10] LEFT-TO-RIGHT ISOLATE..NOMINAL DIGIT SHAPES
-  0xD800 <= code && code <= 0xDFFF || // Cs [2048] <surrogate-D800>..<surrogate-DFFF>
-  0xFEFF === code || // Cf       ZERO WIDTH NO-BREAK SPACE
-  0xFFF0 <= code && code <= 0xFFF8 || // Cn   [9] <reserved-FFF0>..<reserved-FFF8>
-  0xFFF9 <= code && code <= 0xFFFB || // Cf   [3] INTERLINEAR ANNOTATION ANCHOR..INTERLINEAR ANNOTATION TERMINATOR
-  0x1BCA0 <= code && code <= 0x1BCA3 || // Cf   [4] SHORTHAND FORMAT LETTER OVERLAP..SHORTHAND FORMAT UP STEP
-  0x1D173 <= code && code <= 0x1D17A || // Cf   [8] MUSICAL SYMBOL BEGIN BEAM..MUSICAL SYMBOL END PHRASE
-  0xE0000 === code || // Cn       <reserved-E0000>
-  0xE0001 === code || // Cf       LANGUAGE TAG
-  0xE0002 <= code && code <= 0xE001F || // Cn  [30] <reserved-E0002>..<reserved-E001F>
-  0xE0080 <= code && code <= 0xE00FF || // Cn [128] <reserved-E0080>..<reserved-E00FF>
-  0xE01F0 <= code && code <= 0xE0FFF // Cn [3600] <reserved-E01F0>..<reserved-E0FFF>
-  ) {
-      return Control;
-    }
-
   if (0x200C === code || // Cf       ZERO WIDTH NON-JOINER
   0x20D0 <= code && code <= 0x20DC || // Mn  [13] COMBINING LEFT HARPOON ABOVE..COMBINING FOUR DOTS ABOVE
   0x20DD <= code && code <= 0x20E0 || // Me   [4] COMBINING ENCLOSING CIRCLE..COMBINING ENCLOSING CIRCLE BACKSLASH
   0x20E1 === code || // Mn       COMBINING LEFT RIGHT ARROW ABOVE
   0x20E2 <= code && code <= 0x20E4 || // Me   [3] COMBINING ENCLOSING SCREEN..COMBINING ENCLOSING UPWARD POINTING TRIANGLE
   0x20E5 <= code && code <= 0x20F0 || // Mn  [12] COMBINING REVERSE SOLIDUS OVERLAY..COMBINING ASTERISK ABOVE
-  0x2CEF <= code && code <= 0x2CF1 || // Mn   [3] COPTIC COMBINING NI ABOVE..COPTIC COMBINING SPIRITUS LENIS
-  0x2D7F === code || // Mn       TIFINAGH CONSONANT JOINER
-  0xA66F === code || // Mn       COMBINING CYRILLIC VZMET
   0xFE00 <= code && code <= 0xFE0F || // Mn  [16] VARIATION SELECTOR-1..VARIATION SELECTOR-16
-  0xFE20 <= code && code <= 0xFE2F || // Mn  [16] COMBINING LIGATURE LEFT HALF..COMBINING CYRILLIC TITLO RIGHT HALF
-  0x101FD === code || // Mn       PHAISTOS DISC SIGN COMBINING OBLIQUE STROKE
-  0x11A47 === code || // Mn       ZANABAZAR SQUARE SUBJOINER
-  0x1D165 === code || // Mc       MUSICAL SYMBOL COMBINING STEM
-  0x1D167 <= code && code <= 0x1D169 || // Mn   [3] MUSICAL SYMBOL COMBINING TREMOLO-1..MUSICAL SYMBOL COMBINING TREMOLO-3
-  0x1D16E <= code && code <= 0x1D172 || // Mc   [5] MUSICAL SYMBOL COMBINING FLAG-1..MUSICAL SYMBOL COMBINING FLAG-5
-  0x1D17B <= code && code <= 0x1D182 || // Mn   [8] MUSICAL SYMBOL COMBINING ACCENT..MUSICAL SYMBOL COMBINING LOURE
-  0x1D185 <= code && code <= 0x1D18B || // Mn   [7] MUSICAL SYMBOL COMBINING DOIT..MUSICAL SYMBOL COMBINING TRIPLE TONGUE
-  0x1D1AA <= code && code <= 0x1D1AD || // Mn   [4] MUSICAL SYMBOL COMBINING DOWN BOW..MUSICAL SYMBOL COMBINING SNAP PIZZICATO
-  0x1D242 <= code && code <= 0x1D244 || // Mn   [3] COMBINING GREEK MUSICAL TRISEME..COMBINING GREEK MUSICAL PENTASEME
   0x1DA00 <= code && code <= 0x1DA36 || // Mn  [55] SIGNWRITING HEAD RIM..SIGNWRITING AIR SUCKING IN
   0x1DA3B <= code && code <= 0x1DA6C || // Mn  [50] SIGNWRITING MOUTH CLOSED NEUTRAL..SIGNWRITING EXCITEMENT
   0x1DA75 === code || // Mn       SIGNWRITING UPPER BODY TILTING FROM HIP JOINTS
@@ -8535,14 +8458,8 @@ const getGraphemeBreakProperty = code => {
 
   if (0x1F1E6 <= code && code <= 0x1F1FF) {
     // So  [26] REGIONAL INDICATOR SYMBOL LETTER A..REGIONAL INDICATOR SYMBOL LETTER Z
-    return Regional_Indicator;
+    return RegionalIndicator;
   }
-
-  if (0x1D166 === code || // Mc       MUSICAL SYMBOL COMBINING SPRECHGESANG STEM
-  0x1D16D === code // Mc       MUSICAL SYMBOL COMBINING AUGMENTATION DOT
-  ) {
-      return SpacingMark;
-    }
 
   if (0x261D === code || // So       WHITE UP POINTING INDEX
   0x26F9 === code || // So       PERSON WITH BALL
@@ -8577,12 +8494,12 @@ const getGraphemeBreakProperty = code => {
   0x1F93D <= code && code <= 0x1F93E || // So   [2] WATER POLO..HANDBALL
   0x1F9D1 <= code && code <= 0x1F9DD // So  [13] ADULT..ELF
   ) {
-      return E_Base;
+      return EBase;
     }
 
   if (0x1F3FB <= code && code <= 0x1F3FF) {
     // Sk   [5] EMOJI MODIFIER FITZPATRICK TYPE-1-2..EMOJI MODIFIER FITZPATRICK TYPE-6
-    return E_Modifier;
+    return EModifier;
   }
 
   if (0x200D === code) {
@@ -8611,19 +8528,37 @@ const getGraphemeBreakProperty = code => {
   0x1F680 === code || // So       ROCKET
   0x1F692 === code // So       FIRE ENGINE
   ) {
-      return Glue_After_Zwj;
+      return GlueAfterZWJ;
     }
 
   if (0x1F466 <= code && code <= 0x1F469) {
     // So   [4] BOY..WOMAN)
-    return E_Base_GAZ;
+    return EBaseGAZ;
   } //all unlisted characters have a grapheme break property of "Other"
 
 
   return Other;
 };
 
-/* harmony default export */ var split = (splitGraphemes);
+const splitEmojis = str => {
+  let res = [],
+      index = 0,
+      brk,
+      length = str.length;
+
+  while ((brk = nextBreak(str, index)) < length) {
+    res.push(str.substring(index, brk));
+    index = brk;
+  }
+
+  if (index < str.length) {
+    res.push(str.substring(index, length));
+  }
+
+  return res;
+};
+
+/* harmony default export */ var split = (splitEmojis);
 // CONCATENATED MODULE: ./node_modules/react-emoji-component/es/utils/toEmojis.js
 
 
--- src/utils/split.js
+++ src/utils/split.js
@@ -1,15 +1,12 @@
 let CR = 0,
-  LF = 1,
-  Control = 2,
-  Extend = 3,
-  Regional_Indicator = 4,
-  SpacingMark = 5,
-  Other = 11,
-  E_Base = 13,
-  E_Modifier = 14,
-  ZWJ = 15,
-  Glue_After_Zwj = 16,
-  E_Base_GAZ = 17
+  Extend = 1,
+  RegionalIndicator = 2,
+  Other = 3,
+  EBase = 4,
+  EModifier = 5,
+  ZWJ = 6,
+  GlueAfterZWJ = 7,
+  EBaseGAZ = 8
 
 // BreakTypes
 let NotBreak = 0,
@@ -18,17 +15,10 @@ let NotBreak = 0,
   BreakLastRegional = 3,
   BreakPenultimateRegional = 4
 
-const isSurrogate = (str, pos) =>
-  0xd800 <= str.charCodeAt(pos) && str.charCodeAt(pos) <= 0xdbff &&
-  0xdc00 <= str.charCodeAt(pos + 1) && str.charCodeAt(pos + 1) <= 0xdfff
 
 // Private function, gets a Unicode code point from a JavaScript UTF-16 string
 // handling surrogate pairs appropriately
-const codePointAt = (str, idx) => {
-  if (idx === void 0) {
-    idx = 0
-  }
-
+const codePointAt = (str, idx = 0) => {
   let code = str.charCodeAt(idx)
 
   // if a high surrogate
@@ -75,93 +65,81 @@ const codePointAt = (str, idx) => {
 // Private function, returns whether a break is allowed between the
 // two given grapheme breaking classes
 const shouldBreak = (start, mid, end) => {
-  let all = [start].concat(mid).concat([end]),
-    previous = all[all.length - 2],
-    next = end
+  let all = [start]
+  all.push.apply(all, mid)
+  all.push(end)
 
-  // Lookahead termintor for:
-  // GB10. (E_Base | EBG) Extend* ?	E_Modifier
-  let eModifierIndex = all.lastIndexOf(E_Modifier)
+  let previous = all[all.length - 2],
+      next = end
 
-  if (eModifierIndex > 1 &&
-    all.slice(1, eModifierIndex).every(function (c) {
-      return c === Extend
-    }) &&
-    [Extend, E_Base, E_Base_GAZ].indexOf(start) === -1) {
+  // Lookahead terminator for:
+  // GB10. (EBase | EBG) Extend* ?	EModifier
+  let eModifierIndex = all.lastIndexOf(EModifier)
+
+  if (
+    eModifierIndex > 1
+    && Extend !== next
+    && EBase !== next
+    && EBaseGAZ !== next
+    && all.slice(1, eModifierIndex).every(c => c === Extend)
+  ) {
     return Break
   }
 
-  // Lookahead termintor for:
+  // Lookahead terminator for:
   // GB12. ^ (RI RI)* RI	?	RI
   // GB13. [^RI] (RI RI)* RI	?	RI
-  let rIIndex = all.lastIndexOf(Regional_Indicator)
+  let rIIndex = all.lastIndexOf(RegionalIndicator)
 
-  if (rIIndex > 0 &&
-    all.slice(1, rIIndex).every(function (c) {
-      return c === Regional_Indicator
-    }) &&
-    Regional_Indicator !== previous) {
-    if (all.filter(function (c) {
-      return c === Regional_Indicator
-    }).length % 2 === 1) {
+  if (
+    rIIndex > 0
+    && all.slice(1, rIIndex).every(c => c === RegionalIndicator)
+    && RegionalIndicator !== previous
+  ) {
+    if (all.filter(c => c === RegionalIndicator).length % 2 === 1) {
       return BreakLastRegional
     }
     else {
       return BreakPenultimateRegional
     }
   }
-
-  // GB3. CR X LF
-  if (previous === CR && next === LF) {
-    return NotBreak
-  }
   // GB4. (Control|CR|LF) ÷
-  else if (previous === Control || previous === CR || previous === LF) {
-    if (next === E_Modifier && mid.every(function (c) {
-      return c === Extend
-    })) {
-      return Break
-    }
-    else {
-      return BreakStart
-    }
+  if (previous === CR) {
+    return next === EModifier && mid.every(c => c === Extend) ? Break : BreakStart
   }
   // GB5. ÷ (Control|CR|LF)
-  else if (next === Control || next === CR || next === LF) {
+  else if (next === CR) {
     return BreakStart
   }
   // GB9. X (Extend|ZWJ)
   else if (next === Extend || next === ZWJ) {
     return NotBreak
   }
-  // GB9a. X SpacingMark
-  else if (next === SpacingMark) {
-    return NotBreak
-  }
 
-  // GB10. (E_Base | EBG) Extend* ?	E_Modifier
+  // GB10. (EBase | EBG) Extend* ?	EModifier
   let previousNonExtendIndex = all.indexOf(Extend) > -1
     ? all.lastIndexOf(Extend) - 1
     : all.length - 2
-  if ([E_Base, E_Base_GAZ].indexOf(all[previousNonExtendIndex]) > -1 &&
-    all.slice(previousNonExtendIndex + 1, -1).every(function (c) {
-      return c === Extend
-    }) &&
-    next === E_Modifier) {
+
+  if (
+    (EBase === all[previousNonExtendIndex] || EBaseGAZ === all[previousNonExtendIndex])
+    && all.slice(previousNonExtendIndex + 1, -1).every(c => c === Extend)
+    && next === EModifier
+  ) {
     return NotBreak
   }
 
-  // GB11. ZWJ ? (Glue_After_Zwj | EBG)
-  if (previous === ZWJ && [Glue_After_Zwj, E_Base_GAZ].indexOf(next) > -1) {
+  // GB11. ZWJ ? (GlueAfterZWJ | EBG)
+  if (previous === ZWJ && (next === GlueAfterZWJ || next === EBaseGAZ)) {
     return NotBreak
   }
 
   // GB12. ^ (RI RI)* RI ? RI
   // GB13. [^RI] (RI RI)* RI ? RI
-  if (mid.indexOf(Regional_Indicator) > -1) {
+  if (mid.indexOf(RegionalIndicator) > -1) {
     return Break
   }
-  if (previous === Regional_Indicator && next === Regional_Indicator) {
+  if (previous === RegionalIndicator && next === RegionalIndicator) {
     return NotBreak
   }
 
@@ -184,41 +162,32 @@ const nextBreak = (string, index) => {
   }
 
   let prev = getGraphemeBreakProperty(codePointAt(string, index))
-  let mid = [],
-    i = index + 1
+  let mid = [], i = index + 1, atPrev = string.charCodeAt(index)
 
   for (; i < string.length; i++) {
+    const atCurr = string.charCodeAt(i)
     // check for already processed low surrogates
-    if (isSurrogate(string, i - 1)) {
+    if (
+      // is surrogate
+      0xd800 <= atPrev
+      && atPrev <= 0xdbff
+      && 0xdc00 <= atCurr
+      && atCurr <= 0xdfff
+    ) {
       continue
     }
 
     let next = getGraphemeBreakProperty(codePointAt(string, i))
-    if (shouldBreak(prev, mid, next)) {
+
+    if (shouldBreak(prev, mid, next) !== NotBreak) {
       return i
     }
 
     mid.push(next)
+    atPrev = atCurr
   }
+
   return string.length
-}
-
-// Breaks the given string into an array of grapheme cluster strings
-const splitGraphemes = str => {
-  let res = [], index = 0, brk
-
-  while ((
-    brk = nextBreak(str, index)
-  ) < str.length) {
-    res.push(str.slice(index, brk))
-    index = brk
-  }
-
-  if (index < str.length) {
-    res.push(str.slice(index))
-  }
-
-  return res
 }
 
 //given a Unicode code point, determines this symbol's grapheme break property
@@ -229,72 +198,6 @@ const getGraphemeBreakProperty = code => {
   if (0x000D === code) { // Cc       <control-000D>
     return CR
   }
-
-  if (0x000A === code) { // Cc       <control-000A>
-    return LF
-  }
-
-  if (
-    (
-      0x0000 <= code && code <= 0x0009
-    ) || // Cc  [10] <control-0000>..<control-0009>
-    (
-      0x000B <= code && code <= 0x000C
-    ) || // Cc   [2] <control-000B>..<control-000C>
-    (
-      0x000E <= code && code <= 0x001F
-    ) || // Cc  [18] <control-000E>..<control-001F>
-    (
-      0x007F <= code && code <= 0x009F
-    ) || // Cc  [33] <control-007F>..<control-009F>
-
-    0x200B === code || // Cf       ZERO WIDTH SPACE
-    (
-      0x200E <= code && code <= 0x200F
-    ) || // Cf   [2] LEFT-TO-RIGHT MARK..RIGHT-TO-LEFT MARK
-    0x2028 === code || // Zl       LINE SEPARATOR
-    0x2029 === code || // Zp       PARAGRAPH SEPARATOR
-    (
-      0x202A <= code && code <= 0x202E
-    ) || // Cf   [5] LEFT-TO-RIGHT EMBEDDING..RIGHT-TO-LEFT OVERRIDE
-    (
-      0x2060 <= code && code <= 0x2064
-    ) || // Cf   [5] WORD JOINER..INVISIBLE PLUS
-    0x2065 === code || // Cn       <reserved-2065>
-    (
-      0x2066 <= code && code <= 0x206F
-    ) || // Cf  [10] LEFT-TO-RIGHT ISOLATE..NOMINAL DIGIT SHAPES
-    (
-      0xD800 <= code && code <= 0xDFFF
-    ) || // Cs [2048] <surrogate-D800>..<surrogate-DFFF>
-    0xFEFF === code || // Cf       ZERO WIDTH NO-BREAK SPACE
-    (
-      0xFFF0 <= code && code <= 0xFFF8
-    ) || // Cn   [9] <reserved-FFF0>..<reserved-FFF8>
-    (
-      0xFFF9 <= code && code <= 0xFFFB
-    ) || // Cf   [3] INTERLINEAR ANNOTATION ANCHOR..INTERLINEAR ANNOTATION TERMINATOR
-    (
-      0x1BCA0 <= code && code <= 0x1BCA3
-    ) || // Cf   [4] SHORTHAND FORMAT LETTER OVERLAP..SHORTHAND FORMAT UP STEP
-    (
-      0x1D173 <= code && code <= 0x1D17A
-    ) || // Cf   [8] MUSICAL SYMBOL BEGIN BEAM..MUSICAL SYMBOL END PHRASE
-    0xE0000 === code || // Cn       <reserved-E0000>
-    0xE0001 === code || // Cf       LANGUAGE TAG
-    (
-      0xE0002 <= code && code <= 0xE001F
-    ) || // Cn  [30] <reserved-E0002>..<reserved-E001F>
-    (
-      0xE0080 <= code && code <= 0xE00FF
-    ) || // Cn [128] <reserved-E0080>..<reserved-E00FF>
-    (
-      0xE01F0 <= code && code <= 0xE0FFF
-    ) // Cn [3600] <reserved-E01F0>..<reserved-E0FFF>
-  ) {
-    return Control
-  }
-
 
   if (
     0x200C === code || // Cf       ZERO WIDTH NON-JOINER
@@ -312,37 +215,8 @@ const getGraphemeBreakProperty = code => {
       0x20E5 <= code && code <= 0x20F0
     ) || // Mn  [12] COMBINING REVERSE SOLIDUS OVERLAY..COMBINING ASTERISK ABOVE
     (
-      0x2CEF <= code && code <= 0x2CF1
-    ) || // Mn   [3] COPTIC COMBINING NI ABOVE..COPTIC COMBINING SPIRITUS LENIS
-    0x2D7F === code || // Mn       TIFINAGH CONSONANT JOINER
-    0xA66F === code || // Mn       COMBINING CYRILLIC VZMET
-    (
       0xFE00 <= code && code <= 0xFE0F
     ) || // Mn  [16] VARIATION SELECTOR-1..VARIATION SELECTOR-16
-    (
-      0xFE20 <= code && code <= 0xFE2F
-    ) || // Mn  [16] COMBINING LIGATURE LEFT HALF..COMBINING CYRILLIC TITLO RIGHT HALF
-    0x101FD === code || // Mn       PHAISTOS DISC SIGN COMBINING OBLIQUE STROKE
-    0x11A47 === code || // Mn       ZANABAZAR SQUARE SUBJOINER
-    0x1D165 === code || // Mc       MUSICAL SYMBOL COMBINING STEM
-    (
-      0x1D167 <= code && code <= 0x1D169
-    ) || // Mn   [3] MUSICAL SYMBOL COMBINING TREMOLO-1..MUSICAL SYMBOL COMBINING TREMOLO-3
-    (
-      0x1D16E <= code && code <= 0x1D172
-    ) || // Mc   [5] MUSICAL SYMBOL COMBINING FLAG-1..MUSICAL SYMBOL COMBINING FLAG-5
-    (
-      0x1D17B <= code && code <= 0x1D182
-    ) || // Mn   [8] MUSICAL SYMBOL COMBINING ACCENT..MUSICAL SYMBOL COMBINING LOURE
-    (
-      0x1D185 <= code && code <= 0x1D18B
-    ) || // Mn   [7] MUSICAL SYMBOL COMBINING DOIT..MUSICAL SYMBOL COMBINING TRIPLE TONGUE
-    (
-      0x1D1AA <= code && code <= 0x1D1AD
-    ) || // Mn   [4] MUSICAL SYMBOL COMBINING DOWN BOW..MUSICAL SYMBOL COMBINING SNAP PIZZICATO
-    (
-      0x1D242 <= code && code <= 0x1D244
-    ) || // Mn   [3] COMBINING GREEK MUSICAL TRISEME..COMBINING GREEK MUSICAL PENTASEME
     (
       0x1DA00 <= code && code <= 0x1DA36
     ) || // Mn  [55] SIGNWRITING HEAD RIM..SIGNWRITING AIR SUCKING IN
@@ -361,16 +235,8 @@ const getGraphemeBreakProperty = code => {
     return Extend
   }
 
-
   if (0x1F1E6 <= code && code <= 0x1F1FF) { // So  [26] REGIONAL INDICATOR SYMBOL LETTER A..REGIONAL INDICATOR SYMBOL LETTER Z
-    return Regional_Indicator
-  }
-
-  if (
-    0x1D166 === code || // Mc       MUSICAL SYMBOL COMBINING SPRECHGESANG STEM
-    0x1D16D === code // Mc       MUSICAL SYMBOL COMBINING AUGMENTATION DOT
-  ) {
-    return SpacingMark
+    return RegionalIndicator
   }
 
   if (
@@ -443,11 +309,11 @@ const getGraphemeBreakProperty = code => {
       0x1F9D1 <= code && code <= 0x1F9DD
     ) // So  [13] ADULT..ELF
   ) {
-    return E_Base
+    return EBase
   }
 
   if (0x1F3FB <= code && code <= 0x1F3FF) { // Sk   [5] EMOJI MODIFIER FITZPATRICK TYPE-1-2..EMOJI MODIFIER FITZPATRICK TYPE-6
-    return E_Modifier
+    return EModifier
   }
 
   if (0x200D === code) {// Cf       ZERO WIDTH JOINER
@@ -480,16 +346,30 @@ const getGraphemeBreakProperty = code => {
     0x1F680 === code || // So       ROCKET
     0x1F692 === code // So       FIRE ENGINE
   ) {
-    return Glue_After_Zwj
+    return GlueAfterZWJ
   }
 
   if (0x1F466 <= code && code <= 0x1F469) { // So   [4] BOY..WOMAN)
-    return E_Base_GAZ
+    return EBaseGAZ
   }
-
 
   //all unlisted characters have a grapheme break property of "Other"
   return Other
 }
 
-export default splitGraphemes
+const splitEmojis = str => {
+  let res = [], index = 0, brk, length = str.length
+
+  while ((brk = nextBreak(str, index)) < length) {
+    res.push(str.substring(index, brk))
+    index = brk
+  }
+
+  if (index < str.length) {
+    res.push(str.substring(index, length))
+  }
+
+  return res
+}
+
+export default splitEmojis
