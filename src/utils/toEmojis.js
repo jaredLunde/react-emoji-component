a5083fa3df2b546e66ccfab943623101954c5527
Jared Lunde <jared.lunde@gmail.com>
2019-04-01 19:10:00 -0600

    Adds characters to toEmojis skip list

--- src/utils/toEmojis.js
+++ src/utils/toEmojis.js
@@ -5,7 +5,8 @@ import split from './split'
 
 
 // for skipping characters
-const skipRe = /[\p{L}\sA-Za-z!@$%^&()_+\-=\[\]{};':"\\|,.<>\/?]/
+// const skipRe = /[\p{L}\sA-Za-z!@$%^&()_+\-=\[\]{};':"\\|,.<>\/?]/
+const skipRe = /[0-9#@\p{L}\sA-Za-z!@$%^&()_+\-=\[\]{};':"\\|,.<>\/?]/
 
 export default function toEmojis (
   string,
